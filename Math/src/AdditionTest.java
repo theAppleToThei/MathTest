72aa8bcdb58919212a15e06994e246cb6ebd8417
Alex Baratti <alex.techtalk4geeks@gmail.com>
2013-12-29 15:58:14 -0800

    Added user input for addition

--- Math/src/AdditionTest.java
+++ Math/src/AdditionTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
 
@@ -24,5 +26,17 @@ public class AdditionTest
 	{
 		Addition test3 = new Addition();
 		assertEquals(24, test3.add(5, 9, 8, 2));
+	}
+	
+	@Test
+	public void UserInput()
+	{
+		Addition test4 = new Addition();
+		String string1 = JOptionPane.showInputDialog("Enter a number to add.").toString();
+		String string2 = JOptionPane.showInputDialog("Enter another number to add.").toString();
+		int num1 = Integer.parseInt(string1);
+		int num2 = Integer.parseInt(string2);
+		assertEquals(num1 + num2, test4.add(num1, num2));
+		System.out.println(num1 + " + " + num2 + " = " + test4.add(num1, num2));
 	}
 }
