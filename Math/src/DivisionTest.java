267c22c9c2052cb7cc4da7042bdad703d74374cc
Alex Baratti <alex.techtalk4geeks@gmail.com>
2013-12-29 16:02:14 -0800

    Division now has a user input

--- Math/src/DivisionTest.java
+++ Math/src/DivisionTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
 public class DivisionTest
@@ -16,5 +18,17 @@ public class DivisionTest
 	{
 		Division test2 = new Division();
 		assertEquals(2, test2.divide(4, 2));
+	}
+	
+	@Test
+	public void UserInput()
+	{
+		Division test4 = new Division();
+		String string1 = JOptionPane.showInputDialog("Enter a number to divide.").toString();
+		String string2 = JOptionPane.showInputDialog("Enter another number to divide.").toString();
+		int num1 = Integer.parseInt(string1);
+		int num2 = Integer.parseInt(string2);
+		assertEquals(num1 / num2, test4.divide(num1, num2));
+		System.out.println(num1 + " Ö " + num2 + " = " + test4.divide(num1, num2));
 	}
 }
